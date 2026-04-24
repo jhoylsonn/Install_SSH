15934a30e71bcbdd0b9cb66f2b5cb8919e77ea76
jhoylsonn <joilson.joilsonmichell@outlook.com>
2026-04-24 12:54:16 -0400

    Atualização do Script Remoto

--- Install-SSH-Remote.ps1
+++ Install-SSH-Remote.ps1
@@ -1,9 +1,8 @@
-$repo = "https://raw.githubusercontent.com/jhoylsonn/Install_SSH/main"
-
 Write-Host "Baixando instalador SSH..."
 
-$script = "$repo/Script_InstallSSH.ps1"
-Invoke-WebRequest $script -OutFile "$env:TEMP\Script_InstallSSH.ps1"
+$script = "https://raw.githubusercontent.com/jhoylsonn/Install_SSH/refs/heads/main/Script_InstallSSH.ps1"
+
+Invoke-WebRequest -UseBasicParsing $script -OutFile "$env:TEMP\Script_InstallSSH.ps1"
 
 Write-Host "Executando instalador..."
 & "$env:TEMP\Script_InstallSSH.ps1"
